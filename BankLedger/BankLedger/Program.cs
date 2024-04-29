9debd569fb48535cd3c08843a8960c8158f2078c
KrazySaine <159961189+KrazySaine@users.noreply.github.com>
2024-04-29 09:43:51 -0700

    Add files via upload

---
+++ BankLedger/BankLedger/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankLedger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
